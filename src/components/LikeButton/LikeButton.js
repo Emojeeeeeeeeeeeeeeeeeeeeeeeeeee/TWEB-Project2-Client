ff86ec68e25f2269eb676b57a60ab3c054a8fb84
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-12-15 13:19:45 +0100

    Like button, second design

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -5,18 +5,43 @@ import './style.css';
 export class LikeButton extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            likes: 0,
+            status: false
+        };
+        this.toggleLike = this.toggleLike.bind(this);
     }
 
     toggleLike() {
-        
+        // Not liked yet
+        if(!this.state.status) {
+            this.setState((prevState, props) => {
+                return {
+                    likes: prevState.likes + 1,
+                    status: true
+                }
+            });
+        } 
+        // Already liked
+        else {
+            this.setState((prevState, props) => {
+                return {
+                    likes: prevState.likes - 1,
+                    status: false
+                }
+            });
+        }
     }
 
     render() {
+        const buttonLabel = this.state.status ? "button button-like liked" : "button button-like" ;
         return (
-            <button class="like-btn like-review">
-                <i class="fa fa-heart" aria-hidden="true"></i>Like
-            </button>
+            <div>
+                <button className={buttonLabel} onClick={this.toggleLike}>
+                    <i className="fa fa-heart"></i>
+                    <span>Like</span>
+                </button>
+            </div>
         );
     }
 }
