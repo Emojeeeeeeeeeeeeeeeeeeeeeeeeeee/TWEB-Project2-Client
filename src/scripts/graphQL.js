3f0d74348ff41c7d3e91dac0cde791ba87583b5f
mathieu.jee <mathieu.jee@heig-vd.ch>
2019-01-04 14:18:06 +0100

    new queries

--- src/scripts/graphQL.js
+++ src/scripts/graphQL.js
@@ -5,17 +5,59 @@ import axios from 'axios';
 function getMessage (email ){
 }
 
-function createMessage (input) {
- 
+export function getMessages(email, offset) {
+  const queryToSend = {
+    query: 'query GetMessagesFromDB($email: String!, $offset: Int!){getMessagesFromDB(email: $email, offset: $offset){id}}',
+    variables: {
+      email: email,
+      offset: offset,
+    },
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }
+
+  return axios( {
+    url: 'http://localhost:5000/graphql', 
+    method: 'get',
+    data: queryToSend
+  })
+  .then(response => response.data)
+  .then(response => console.log(response))
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+export function createMessage (author, content) {
+  const queryToSend = {
+    query: 'query CreateMessage($author: String!, $content: String!){createMessage(author: $author, content: $content){id}}',
+    variables: {
+      author: author,
+      content: content,
+    },
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }
+
+  return axios({
+    url: 'http://localhost:5000/graphql',
+    method: 'post',
+    data: queryToSend
+  })
+  .then(response => console.log(response))
+  .catch(error => {
+    console.error(error);
+  })
 }
 
 function updateMessage ( id, input ) {
 
 }
 
-
 export function createUser (user){
-  let queryToSend = {
+  const queryToSend = {
     query: 'query CreateUser($input: UserInput!){createUser(input: $input){id}}',
     variables: {
       input: {
