411c8a65661d06df4533c48f8ce1238d36392063
Florent <shinopill@gmail.com>
2019-01-04 13:59:26 +0100

    merging

--- src/scripts/graphQL.js
+++ src/scripts/graphQL.js
@@ -1,18 +1,57 @@
 let fetch = require('node-fetch')
 
-function getMessage (email ) {
+
+function getMessages(email) 
+{
+  const queryToSend = {
+    
+    query: "query GetMessages($email: String!){getMessages(email: $email){id}}",
+    variables: {
+      email: email
+      }
+    
+  }
+  
+  fetch('http://localhost:5000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify(queryToSend)
+  })
+  .then(r => r.json())
+  .then(r => console.log('data returned:', r))
+  .catch(err=> console.log("erreurs :", err))
 }
 
-function getMessages () {
- 
-}
 
 function createMessage (input) {
- 
+  const queryToSend = {
+    query: "mutation CreateMessage($input: MessageInput){createMessage(input: $input){id}}",
+    variables: {
+      input: {
+      content: input.content,
+      author: input.author,
+      }
+    }
+  }
+  
+  fetch('http://localhost:5000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify(queryToSend)
+  })
+  .then(r => r.json())
+  .then(r => console.log('data returned:', r))
+  .catch(err=> console.log("erreurs :", err))
 }
 
-function updateMessage ( id, input ) {
-
+function deleteMessage(){
+  
 }
 
 function createUser (user){
@@ -42,8 +81,21 @@ function createUser (user){
   
 }
 
+let t = {
+  author:"coucou@test.com",
+  content:"plz send only one"
+}
+
+createUser({
+  email:"coucou2@coucou.com",
+  password:"test",
+  username:"test"
+})
+//createMessage(t);
+//let email = "toto@tutu.tata"
+//getMessages(email)
 module.exports = {
   createUser,
   createMessage,
-  getMessage
+  getMessages
 }
