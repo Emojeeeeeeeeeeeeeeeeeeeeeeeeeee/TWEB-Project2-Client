bfc14a45a33687006c342019f10f1e0d11470a11
mathieu.jee <mathieu.jee@heig-vd.ch>
2019-01-15 21:59:29 +0100

    blank screen

--- src/components/WaitingPage.js
+++ src/components/WaitingPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
 
 export const WaitingPage = () => {    
+
     return (
+        /*<Loader
+            type="Grid"
+            color="#00BFFF"
+            height="100"
+            width="100" 
+            />   */
         <div>
-            LOADING...
+            
         </div>
     );
 };
