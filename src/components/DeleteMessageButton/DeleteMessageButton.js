90f9030ffbf09a2bd67ff057158ac1e4be5ce518
mathieu.jee <mathieu.jee@heig-vd.ch>
2019-01-16 16:43:20 +0100

    delete message confirmation popup

--- src/components/DeleteMessageButton/DeleteMessageButton.js
+++ src/components/DeleteMessageButton/DeleteMessageButton.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import { MDBIcon } from 'mdbreact';
+import { confirmAlert } from 'react-confirm-alert'; 
 
 import { deleteMessage } from '../../scripts/graphQL';
 
+import 'react-confirm-alert/src/react-confirm-alert.css' 
 import './style.css';
-
+ 
 export class DeleteMessageButton extends Component {
     constructor(props) {
         super(props);
         this.handleDelete = this.handleDelete.bind(this);
+        this.deleteConfirmation = this.deleteConfirmation.bind(this);
+    }
+
+    deleteConfirmation() {
+        confirmAlert({
+            customUI: ({ onClose}) => {
+                return (
+                    <div className="custom-ui">
+                        <h1><span role="img" aria-label="deleteCross">❌</span> confirmation</h1>
+                        <p>
+                            Are you sure to delete your 
+                            <span role="img" aria-label="messageBubble">💬</span> 
+                            <span role="img" aria-label="questionMark">❓</span>
+                        </p>
+                        <button className="confirmButton" onClick={this.handleDelete}>
+                            <span role="img" aria-label="thumbup">👍</span>
+                        </button>
+                        <button className="confirmButton" onClick={onClose}>
+                            <span role="img" aria-label="thumbdown">👎</span>
+                        </button>
+                    </div>
+                )
+            }
+        })
     }
 
     handleDelete() {
@@ -18,7 +44,7 @@ export class DeleteMessageButton extends Component {
 
     render() {
         return(
-            <button className="deleteButton" onClick={this.handleDelete}> 
+            <button className="deleteButton" onClick={this.deleteConfirmation}> 
                 <MDBIcon icon="close" size="sm"/>
             </button>
         );
