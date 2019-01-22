c9b70784fb3ccc80f493d5928d0a7a13b353581b
unknown <olivier.kopp@heig-vd.ch>
2019-01-23 00:34:55 +0100

    test

--- src/scripts/graphQL.js
+++ src/scripts/graphQL.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://emojee-client.herokuapp.com/graphql';
+const URL = 'https://emojee-server.herokuapp.com/graphql';
 
 export function createMessage (authorId, content) {
   const queryToSend = {
